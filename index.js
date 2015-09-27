f07ef0f10547a63d078a53d46edf8c0218e85443
hollowdoor <hollowdoor99@gmail.com>
2015-09-27 04:50:05 -0500

    Simple comment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var shell = require('./shell'),
     state = require('carefree-states');
-
+/*
+git remote add origin https://github.com/hollowdoor/gyre.git
+git push -u origin master
+*/
 module.exports = {
     shell: shell,
     state: state
